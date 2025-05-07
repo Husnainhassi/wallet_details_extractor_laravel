314f8cb156f2610317a11d330d63b45383af0caf
Afaq Khursheed <afaqkhurshid@axiscodingsoultions.com>
2025-05-07 21:07:03 +0500

    fix: cargo changes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\WalletAnalyzerController;
+use App\Http\Controllers\DefaultController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Http;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/wallet-list', [WalletAnalyzerController::class, 'walletList'])->name('wallet_list');
+Route::get('/list', [DefaultController::class, 'list'])->name('list');
+Route::get('/excel-import', [DefaultController::class, 'excelImport'])->name('excel_import');
